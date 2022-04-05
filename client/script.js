8fd6f533271671d007f7f383d8b3776534a838ee
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-04-05 13:19:59 -0400

    set up socket connection for multiple clients

--- client/script.js
+++ client/script.js
@@ -1,1 +1,41 @@
-console.log("client");
+import { io } from 'socket.io-client';
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () =>{
+  displayMessage(`You connected with id : ${socket.id}`)
+  
+})
+
+socket.on("receive-message", (message) => {
+  displayMessage(message);
+});
+
+
+const form = document.getElementById("form");
+const joinButton = document.getElementById("join-room");
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  const message = document.getElementById("message");
+  if (message === "") return;
+  displayMessage(message.value);
+  socket.emit("send-message", message.value);
+
+  message.value = "";
+}
+
+function displayMessage(message) {
+  const div = document.createElement("div")
+  div.textContent = message;
+  const container = document.getElementById("container");
+  container.append(div);
+}
+
+joinButton.addEventListener("click", () => {
+  const roomName = document.getElementById("room");
+  console.log(roomName.value);
+});
--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
 console.log("Hello");
+const port = 3000;
+
+const io = require("socket.io")(port, {
+  cors: {
+    origin: ["http://localhost:8080"]
+  }
+});
+
+io.on("connection", socket => {
+  console.log(socket.id);
+  
+  socket.on("send-message", (message) => {
+    socket.broadcast.emit("receive-message", message)
+  })
+})
